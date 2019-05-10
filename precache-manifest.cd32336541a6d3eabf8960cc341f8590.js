30adc11d8e713cb529b1936a05c9f19524737b03
Matthew Viego <mjv35@pitt.edu>
2019-05-10 13:53:20 -0400

    Updates

---
+++ precache-manifest.cd32336541a6d3eabf8960cc341f8590.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriendsApp/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "40cbab871f78f77db683",
+    "url": "/RoboFriendsApp/static/js/runtime~main.9967c245.js"
+  },
+  {
+    "revision": "e8ec153ce08ba629af39",
+    "url": "/RoboFriendsApp/static/js/main.22f0ebe3.chunk.js"
+  },
+  {
+    "revision": "bdc162d470ce2366fb1b",
+    "url": "/RoboFriendsApp/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "e8ec153ce08ba629af39",
+    "url": "/RoboFriendsApp/static/css/main.a47844e3.chunk.css"
+  },
+  {
+    "revision": "bdc162d470ce2366fb1b",
+    "url": "/RoboFriendsApp/static/css/2.21cf172f.chunk.css"
+  },
+  {
+    "revision": "3e87fa4edcbb5531abf707c2c447d96f",
+    "url": "/RoboFriendsApp/index.html"
+  }
+];
